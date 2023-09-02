489fe017acd7d5f905d283beb20fcee0c7dd418f
diegoziraldo <ziraldodiego@gmail.com>
2023-09-02 16:42:32 -0300

    020923

--- arrays.py
+++ arrays.py
@@ -1,1 +1,4 @@
-a
+texto = 'hola mundo'
+
+for texto in range(2,10):
+     print(texto)
---
+++ diccionarios.py
@@ -0,0 +1,18 @@
+diccionario = {
+    "1" : "Uno",
+    "2" : "Dos",
+    "3" : "Tres",
+    "4" : "Cuatro",
+    "5" : "Cinco",
+    "6" : "Seis",
+    "7" : "Siete",
+    "8" : "Ocho",
+    "9" : "Nueve",
+    "10" : "Diez",
+
+}
+
+numero = input('Ingrese un numero del 1 al diez')
+
+print (diccionario[numero])
+

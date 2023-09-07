99e51d314a11ddefdbdaae973b971767f6651f88
diegoziraldo <ziraldodiego@gmail.com>
2023-09-07 14:33:20 -0300

    070923

--- encriptador.py
+++ encriptador.py
@@ -7,9 +7,35 @@ archivo = open('texto.txt', 'r')
 print(archivo.read())
 '''
 
-def encriptar():
-    print('Funcion para encriptar')
 
-def desencriptar():
-    print('Funcion para desencriptar')
+def encriptar(texto):
+    print(f'Texto para encriptar {texto}')
+    textoFinal = ''
+    for letra in texto:
+        textoFinal += letra + 'x'
+    print(textoFinal)
+
+#Esta funcion agregara una letra x despues de cada letra
+#El texto se enviara en los parametros
+#encriptar('123ddf456')
+
+
+def desencriptar(texto):
+    print(f'Texto para desencriptar {texto}')
+    textoFinal = ''
+    contador = 0
+    for letra in texto:
+        if contador % 2 == 0:
+            textoFinal += letra
+        contador +=1
+    print(f'Texto desencriptado es: {textoFinal}')
+        
+#Lo que hace esta funcion es despues de cada letra par no agregarle una letra al textoFinal
+#entonces como cada letra par tiene una x este no se va a agregar.
+#desencriptar('prueba de texto')
+
+
+
+ 
+ 
     
